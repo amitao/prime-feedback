41ddf7ede3839e4fe3fcf3fcf1ab2ce958fdbe7c
Amie Thao <amie.c.thao@gmail.com>
2018-12-08 12:57:13 -0600

    create ADD_reducer

--- src/components/FeedBackOne/FeedBackOne.jsx
+++ src/components/FeedBackOne/FeedBackOne.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class FeedBackOne extends Component {
+
+  handleClick = () => {
+    console.log('Feedback one has been clicked');
+    
+  }
+
   render () {
     return (
       <div>
         <h1>FeedBack One</h1>
+        <button onClick={this.handleClick}>NEXT</button>
       </div>
     )
   }
 }
 
-export default FeedBackOne;
+export default connect()(FeedBackOne);
