a006215065c08aee58396a5f539622c406fc7db5
Amie Thao <amie.c.thao@gmail.com>
2018-12-08 13:37:23 -0600

    add buttons and routes to components

--- src/components/SubmitFeedBack/SubmitFeedBack.jsx
+++ src/components/SubmitFeedBack/SubmitFeedBack.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 
 class SubmitFeedBack extends Component {
+
+  handleClick = () => {
+    console.log('Submit has been clicked');
+  }
+
+
   render () {
     return (
       <div>
         <h1>Submit FeedBack</h1>
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
