bbc0694f2585b736f1552bcd9e7ad25742d9c264
Amie Thao <amie.c.thao@gmail.com>
2018-12-11 15:35:28 -0600

    Updates

---
+++ precache-manifest.4e885807a9ab593b896e7eb33ccec65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65f6b738e815759e3464",
+    "url": "/prime-feedback/static/css/main.42894a72.chunk.css"
+  },
+  {
+    "revision": "65f6b738e815759e3464",
+    "url": "/prime-feedback/static/js/main.65f6b738.chunk.js"
+  },
+  {
+    "revision": "8018ac403c1686c1fae3",
+    "url": "/prime-feedback/static/js/1.8018ac40.chunk.js"
+  },
+  {
+    "revision": "23a7cc8b85d21aa8f5d0",
+    "url": "/prime-feedback/static/js/runtime~main.23a7cc8b.js"
+  },
+  {
+    "revision": "d071011cc30f4f5f1eb783b0dcd905c6",
+    "url": "/prime-feedback/index.html"
+  }
+];
